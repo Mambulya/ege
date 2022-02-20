14000f7ea8c4051c5abcdca3520af99b7cd2843b
Mambulya <68922098+Mambulya@users.noreply.github.com>
2022-02-21 00:21:22 +0300

    Update EABEABEA.py

--- №24/EABEABEA.py
+++ №24/EABEABEA.py
@@ -4,7 +4,7 @@ task:
 C, D, E. Найдите максимальную длину цепочки вида EABEABEABE... (составленной из фрагментов
 EAB, последний фрагмент может быть неполным).
 
-doc: XEAEABXEABEABEABEA
+tests: XEAEABXEABEXABEA
 """
 
 with open("C:/Users/admin/Downloads/for24.txt") as f:
@@ -12,14 +12,15 @@ with open("C:/Users/admin/Downloads/for24.txt") as f:
     answer = 0
     maxAnswer = 0
     for i in range(len(string)):
+
         if any((answer % 3 == 0 and string[i] == 'E', answer % 3 == 1 and string[i] == 'A',
                 answer % 3 == 2 and string[i] == 'B')):
             answer += 1
             if answer >= 3:
                 maxAnswer = max(maxAnswer, answer)
-                
-            # logging   
-            print("{}, answer - {}, maxAnser - {}".format(string[i], answer, maxAnswer))
+
+        elif string[i] == 'E':
+            answer = 1
 
         else:
             answer = 0
