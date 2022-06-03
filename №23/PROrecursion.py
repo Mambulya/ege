a9cdecc65835824a2b3bbaba4915473873cdaac6
Mambulya <68922098+Mambulya@users.noreply.github.com>
2022-06-04 00:01:07 +0300

    обязательное и избегательное число
    Исполнитель Фибо преобразует число на экране.
    У исполнителя есть две команды, которым присвоены номера:
    1. Прибавить 1
    2. Прибавить 2
    Сколько существует программ, которые преобразуют исходное число 2 в число 18 и при этом траектория вычислений содержит число 9 и не содержит числа 14?

---
+++ №23/PROrecursion.py
@@ -0,0 +1,12 @@
+"""
+x: start number
+y: aim number
+"""
+def f(x, y):
+    if x == y:
+        return 1
+    if x > y or x == 14:
+        return 0
+    return f(x + 1, y) + f(x + 2, y)
+
+print(f(2, 9) * f(9, 18))
