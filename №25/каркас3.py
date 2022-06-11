77aaf18fd59fb1bb7be4d7b364f179794a01e45c
Mambulya <68922098+Mambulya@users.noreply.github.com>
2022-06-12 02:54:46 +0300

    нечётное кол-во делителей
    ЗАДАНИЕ:
    Назовём нетривиальным делителем натурального числа его делитель, не равный единице и самому числу. Например, у числа 6 есть два нетривиальных делителя: 2 и 3. Найдите все натуральные числа, принадлежащие отрезку [4234679; 10157813] и имеющие ровно три нетривиальных делителя. Для каждого найденного числа запишите в ответе его наибольший нетривиальный делитель. Ответы расположите в порядке возрастания.

---
+++ №25/каркас3.py
@@ -0,0 +1,34 @@
+def NumberOFdivisors(n):
+    """
+    finds number of divisors on the number n
+
+    :param n: a number which divisors are found
+    :return: list: number of divisors of n (counting 1 and n), every divisor
+    """
+    if pow(n, 0.5) != int(pow(n, 0.5)): # НЕЧЁТНОЕ КОЛИЧЕСТВО ДЕЛИТЕЛЕЙ МОЖЕТ БЫТЬ ЛИШЬ У НАТУРАЛЬНЫХ КВАДРАТОВ! из-за доп делителя (строчка 24)
+        return [0]
+
+    divisorsNumber = 2  # take into account the divisors like 1 and n
+    d = 2  # is a divisor
+    allDivisors = [1, n]
+
+    while d * d < n:
+        if n % d == 0:  # is this d a real divisor?
+            divisorsNumber += 2  # every divisor has their own couple
+            if divisorsNumber > 5:
+                break
+            allDivisors.append(d)
+            allDivisors.append(n // d)
+        d += 1
+
+    if d * d == n:  # if n is a natural number squared (ex: 1, 4, 9 ...),
+        # it has an additional divisor n**0.5
+        divisorsNumber += 1
+        allDivisors.append(d)
+
+    return [divisorsNumber] + sorted(allDivisors)
+
+for n in range(4234679, 10157813):
+    res = NumberOFdivisors(n)
+    if res[0] == 5:
+        print("{}: {}".format(n, res))
